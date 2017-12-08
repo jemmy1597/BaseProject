052228737183871e73999179d32c7bd98f933cfd
jemmy1597 <jemmy1597@gmail.com>
2017-12-08 20:30:24 +0800

    plugin test

--- src/test/java/com/gitHub/jemmy1597/QAQRcodeTest.java
+++ src/test/java/com/gitHub/jemmy1597/QAQRcodeTest.java
@@ -14,9 +14,20 @@ public class QAQRcodeTest {
         ///////////第二階段
         int temp = qaq.haha(1);
 
-        /////////第三階段
+        /////////第三階段w
         Assert.assertEquals(1 , temp);
 
     }
+    @Test
+    public void haha1() throws Exception {
+        ////////////第一階段
+        QAQRcode qaq = new QAQRcode();
 
+        ///////////第二階段
+        int temp = qaq.haha(0);
+
+        /////////第三階段
+        Assert.assertEquals(0 , temp);
+
+    }
 }
